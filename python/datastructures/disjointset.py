10e8a55a746234d7c6b6ecc57b047e39e2030094
DanzTheDeadly <danzerr@gmail.com>
2023-05-31 23:14:29 +0400

    add disjointset optimized

--- python/datastructures/disjointset.py
+++ python/datastructures/disjointset.py
@@ -19,3 +19,30 @@ class DisjointSet:
 
     def connected(self, x, y):
         return self.find(x) == self.find(y)
+    
+
+class DisjointSetOptimized:
+    def __init__(self, size):
+        self.parent = list(range(size))
+    
+    def __repr__(self) -> str:
+        return str(self.parent)
+    
+    def find(self, x):
+        to_link = []
+        while x != self.parent[x]:
+            to_link.append(x)
+            x = self.parent[x]
+        for idx in to_link:
+            self.parent[idx] = x
+        return x
+    
+    def union(self, x, y):
+        parentX = self.find(x)
+        parentY = self.find(y)
+        if parentX != parentY:
+            self.parent[parentY] = parentX
+        print(self, (x,y))
+
+    def connected(self, x, y):
+        return self.find(x) == self.find(y)
