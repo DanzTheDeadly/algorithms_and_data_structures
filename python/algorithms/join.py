cc5ac9345fdca2b74a2d2a27f222c710e6b373b5
DanzTheDeadly <danzerr@gmail.com>
2023-06-11 17:02:39 +0400

    change join delimiter

--- python/algorithms/join.py
+++ python/algorithms/join.py
@@ -2,7 +2,7 @@ def hash_join (left_file, right_file):
     def build_hashmap (datafile):
         hashmap = {}
         for line in datafile:
-            key, *value = line.strip().split('\t')
+            key, *value = line.strip().split(';')
             if not hashmap.get(key):
                 hashmap[key] = [''.join(value)]
             else:
@@ -16,11 +16,11 @@ def hash_join (left_file, right_file):
     l = build_hashmap(left)
 
     for r_line in right:
-        r = tuple(r_line.strip().split('\t'))
+        r = tuple(r_line.strip().split(';'))
         l_values = l.get(r[0])
         if l_values:
             for l_value in l_values:
-                joined_row = '{}\t{}\t{}\n'.format(r[0], l_value, r[1])
+                joined_row = '{};{};{}\n'.format(r[0], l_value, r[1])
                 out.write(joined_row)
     right.seek(0)
     left.close()
@@ -34,11 +34,11 @@ def nested_loop_join (left_file, right_file):
     out = open('result.txt', 'w')
 
     for l_line in left:
-        l = tuple(l_line.strip().split('\t'))
+        l = tuple(l_line.strip().split(';'))
         for r_line in right:
-            r = tuple(r_line.strip().split('\t'))
+            r = tuple(r_line.strip().split(';'))
             if l[0] == r[0]:
-                joined_row = '{}\t{}\t{}\n'.format(l[0], l[1], r[1])
+                joined_row = '{};{};{}\n'.format(l[0], l[1], r[1])
                 out.write(joined_row)
         right.seek(0)
     left.close()
